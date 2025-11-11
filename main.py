de2c1c329865e04319e1eddb55d128d764effa75
vextes <sjfrang@gmail.com>
2025-11-11 16:24:28 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key = api_key)
+
+    # Get prompt from command line
+    try:
+        text_prompt = sys.argv[1]
+    except:
+        print("ERROR: No prompt found")
+        sys.exit(1)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=text_prompt)])
+    ]
+    response = client.models.generate_content(
+        model = 'gemini-2.0-flash-001',
+        contents = messages
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
