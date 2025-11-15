269ba37e1cd10ba0146a72aa220966bd70d0b33f
vextes <sjfrang@gmail.com>
2025-11-14 19:08:21 -0600

    add specific file read capability

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,2 +1,23 @@
+import os
+from config import MAX_CHARS
+
 def get_file_content(working_directory, file_path):
-    pass
+    # path setup
+    wd_path = os.path.abspath(working_directory)
+    file_abs_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    # return errors for invalid directory inputs
+    if not file_abs_path.startswith(wd_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(file_abs_path):
+        f'Error: File not found or is not a regular file: "{file_path}"'
+
+    # logic
+    try:
+        with open(file_abs_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        if os.path.getsize(file_abs_path) > MAX_CHARS:
+            file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
+        return file_content_string
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,6 +1,25 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+
 
 def test():
+    test1 = get_file_content("calculator", "main.py")
+    print("Result for test1:")
+    print(f"{test1}\n")
+
+    test2 = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for test2:")
+    print(f"{test2}\n")
+    
+    test3 = get_file_content("calculator", "/bin/cat")
+    print("Result for test3:")
+    print(f"{test3}\n")
+    
+    test4 = get_file_content("calculator", "pkg/does_not_exist.py")
+    print("Result for test4:")
+    print(f"{test4}\n")
+
+def test_file_info():
     test1 = get_files_info("calculator", ".")
     print("Result for current directory:")
     print(f"{test1}\n")
